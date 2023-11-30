a8cc6cccf42802e2e2bdfe3468ee7afbac7d07f2
mhsamie <sfatima7720@gmail.com>
2023-11-30 16:20:39 +0330

    finnal

--- js/script.js
+++ js/script.js
@@ -1,36 +1,35 @@
-const hamburgerIcon = document.querySelector(".hamberger-icon");
-const hamburgerMenu = document.querySelector(".hamberger-menu");
-const crossIcon = document.querySelector(".cross");
-const showMoreMenu = document.querySelector("#show-more-menu");
-const showProductMenu = document.querySelector("#show-product-menu");
+const hamburgerIcon = document.querySelector('.hamburger-icon');
+const hamburgerMenu = document.querySelector('.hamburger-menu');
+const crossIcon = document.querySelector('.cross-icon');
+const showMoreMenu = document.querySelector('#show-more-menu');
+const showProductMenu = document.querySelector('#show-product-menu');
 
-hamburgerIcon.addEventListener("click", function () {
-  console.log(hamburgerMenu.style.transform);
-  hamburgerMenu.classList.add("show-hamburger-menu");
+hamburgerIcon.addEventListener('click', function () {
+  hamburgerMenu.classList.add('show-hamburger-menu');
 });
 
-crossIcon.addEventListener("click", function () {
-  hamburgerMenu.classList.remove("show-hamburger-menu");
+crossIcon.addEventListener('click', function () {
+  hamburgerMenu.classList.remove('show-hamburger-menu');
 });
 
-showMoreMenu.addEventListener("mouseenter", function (e) {
+showMoreMenu.addEventListener('mouseenter', function (e) {
   const ul = e.target.childNodes[5];
-  ul.style.animation = "showUp 0.2s 1";
-  ul.style.display = "block";
+  ul.style.animation = 'showUp 0.35s 1';
+  ul.style.display = 'block';
 });
 
-showMoreMenu.addEventListener("mouseleave", function (e) {
+showMoreMenu.addEventListener('mouseleave', function (e) {
   const ul = e.target.childNodes[5];
-  ul.style.display = "none";
+  ul.style.display = 'none';
 });
 
-showProductMenu.addEventListener("mouseenter", function (e) {
+showProductMenu.addEventListener('mouseenter', function (e) {
   const div = e.target.childNodes[5];
-  div.style.animation = "showUp 0.2s 1";
-  div.style.display = "flex";
+  div.style.animation = 'showUp 0.35s 1';
+  div.style.display = 'flex';
 });
 
-showProductMenu.addEventListener("mouseleave", function (e) {
+showProductMenu.addEventListener('mouseleave', function (e) {
   const div = e.target.childNodes[5];
-  div.style.display = "none";
+  div.style.display = 'none';
 });
